7c54ef82c66045ee9e5b66179cdcbb6308a6bd28
Ng Yibin <ngyibin@gmail.com>
2025-10-09 15:46:54 +0800

    add crewai files

---
+++ StockDirectionPrediction_CrewAI/readme.md
@@ -0,0 +1,14 @@
+# Stock recommendation using CrewAI
+
+This code is used in a blog post [here](https://medium.com/@ngyibin/multi-agent-workflows-using-autogen-for-stock-recommendation-d9a91c3e7eef).
+
+To change the input data, comment out the appropriate lines in the code:
+```
+#### Input parameters ##########
+# company = 'alphabet'
+# company = 'intel'
+company = 'at&t'
+################################
+```
+
+
---
+++ StockDirectionPrediction_CrewAI/utils.py
@@ -0,0 +1,41 @@
+# Add your utilities or helper functions to this file.
+
+import os
+from dotenv import load_dotenv, find_dotenv
+
+# these expect to find a .env file at the directory above the lesson.                                                                                                                     # the format for that file is (without the comment)                                                                                                                                       #API_KEYNAME=AStringThatIsTheLongAPIKeyFromSomeService
+def load_env():
+    _ = load_dotenv(find_dotenv())
+
+def get_openai_api_key():
+    load_env()
+    api_key = os.getenv("OPENAI_API_KEY")
+    return api_key
+
+def get_serper_api_key():
+    load_env()
+    api_key = os.getenv("SERPER_API_KEY")
+    return api_key
+
+
+# break line every 80 characters if line is longer than 80 characters
+# don't break in the middle of a word
+def pretty_print_result(result):
+  parsed_result = []
+  for line in result.split('\n'):
+      if len(line) > 80:
+          words = line.split(' ')
+          new_line = ''
+          for word in words:
+              if len(new_line) + len(word) + 1 > 80:
+                  parsed_result.append(new_line)
+                  new_line = word
+              else:
+                  if new_line == '':
+                      new_line = word
+                  else:
+                      new_line += ' ' + word
+          parsed_result.append(new_line)
+      else:
+          parsed_result.append(line)
+  return "\n".join(parsed_result)
